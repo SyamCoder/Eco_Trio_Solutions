6d2c9b5484d5d9b19b85be94bd23427799a88017
Chaitanya-2305 <annaluruchaitanya@gmail.com>
2025-07-29 20:21:35 +0530

    Fix static files and database settings without secrets

--- Eco_Trio_Main/settings.py
+++ Eco_Trio_Main/settings.py
@@ -159,12 +159,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'Eco_Trio_Sub/statics'),
+    BASE_DIR / 'Eco_Trio_Sub' / 'static',
 ]
 
 # Whitenoise settings
