17e0465b42b71381ed62edff02ef418ec247248b
Chaitanya-2305 <annaluruchaitanya@gmail.com>
2025-07-29 22:01:38 +0530

    Fix: use Eco_Trio_Main.settings wsgi

--- Eco_Trio_Sub/wsgi.py
+++ Eco_Trio_Sub/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Eco_Trio_Main.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Eco_Trio_Sub.settings')
 
 application = get_wsgi_application()
