806c58d10674993db124930ac408618708715aa6
Chaitanya-2305 <annaluruchaitanya@gmail.com>
2025-08-15 21:51:53 +0530

    email UPDATES

--- Eco_Trio_Main/settings.py
+++ Eco_Trio_Main/settings.py
@@ -367,7 +367,7 @@ EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
-ADMIN_EMAIL = 'ecotriosolutionweb@gmail.com'
+ADMIN_EMAIL = os.environ.get('EMAIL_HOST_USER')
 
 # Authentication settings
 SOCIALACCOUNT_LOGIN_ON_GET = True
